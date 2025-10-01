14569c2e49c61d1e18d4ddf2ee431bcc49927717
che5ya <che5ya@naver.com>
2025-10-02 07:03:58 +0900

    # REMOVE - delete legacy files

--- old/src_old/isFunction.js
+++
@@ -1,9 +0,0 @@
-/**
- * Checks if the given argument is a function.
- * @param {any} func - The argument to check.
- * @returns {boolean} Returns true if the argument is a function, otherwise false.
- */
-
-export default function isFunction(func) {
-    return (typeof func == 'function');
-};

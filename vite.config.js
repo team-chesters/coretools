691aa4643623f93fef9c2bf8d480670edd0597ea
che5ya <che5ya@naver.com>
2025-10-02 21:57:38 +0900

    # EDIT - edit umd build setting

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,13 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, './coretools.default.js'),
       name: 'coretools',
-      fileName: (format) => `coretools.${format}.js`
+      fileName: (format) => `coretools.${format}.js`,
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      output: {
+        exports: "named"
+      }
     }
-  },
-  output: {
-    exports: "named"
   }
 });
